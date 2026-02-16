2542f3becd48d92d58e8e99fcf3143eeb133f48d
SRINUNAIK208 <srinaik208@gmail>
2026-02-17 00:41:50 +0530

    TerraformAws

--- frontend_alb/local.tf
+++ frontend_alb/local.tf
@@ -2,6 +2,7 @@ locals {
     acm_certificate_arn= data.aws_ssm_parameter.aws_acm_certificate.value 
     public_subnet_ids = split(",",data.aws_ssm_parameter.public_subnet_ids.value)
     frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = {
         project = var.project
         environment= var.environment
