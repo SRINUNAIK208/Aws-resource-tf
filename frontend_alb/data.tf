4a671e6b274b1ded3c9dbd30a35847806524dd9c
SRINUNAIK208 <srinaik208@gmail>
2026-02-17 00:38:44 +0530

    TerraformAws

--- frontend_alb/data.tf
+++ frontend_alb/data.tf
@@ -1,3 +1,14 @@
 data "aws_ssm_parameter" "aws_acm_certificate" {
   name = name  = "/${var.project}/${var.environment}/aws_acm_certificate"
 }
+data "aws_ssm_parameter" "vpc_id" {
+  name =  "/${var.project}/${var.environment}/vpc_id"
+}
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/${var.project}/${var.environment}/public_subnet_ids"
+}
+
+data "aws_ssm_parameter" "frontend_alb_sg_id" {
+  name ="/${var.project}/${var.environment}/frontend_alb_sg_id"
+}
--- frontend_alb/local.tf
+++ frontend_alb/local.tf
@@ -1,3 +1,11 @@
 locals {
     acm_certificate_arn= data.aws_ssm_parameter.aws_acm_certificate.value 
+    public_subnet_ids = split(",",data.aws_ssm_parameter.public_subnet_ids.value)
+    frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
+    common_tags = {
+        project = var.project
+        environment= var.environment
+        terraform = true
+    }
+   
 }
