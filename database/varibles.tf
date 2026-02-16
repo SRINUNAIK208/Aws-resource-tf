7a05cf4a0b3424fa047760e170b23306f7ce946c
SRINUNAIK208 <srinaik208@gmail>
2026-02-16 22:10:53 +0530

    TerraformAws

--- database/varibles.tf
+++ database/varibles.tf
@@ -4,3 +4,10 @@ variable "project" {
 variable "environment"{
     default = "dev"
 }
+variable "zone_id" {
+    default = "Z02430753K0CVZ8NBSTXB"
+}
+
+variable "zone_name"{
+    default = "srinunayak.online"
+}
